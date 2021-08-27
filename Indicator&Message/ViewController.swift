caf90b42a702974585bf83a46cd5bd58fe9773f8
Misha Dovhiy <hi@dovhiy.com>
2021-08-27 23:06:58 +0300

    Initial Commit

---
+++ Indicator&Message/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Indicator&Message
+//
+//  Created by Mikhailo Dovhyi on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
